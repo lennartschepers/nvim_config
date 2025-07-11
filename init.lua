35fff15b13cbb6a57a9258e16c81bdf0ed0444d6
Lennart <lenschepers@gmail.com>
2025-07-11 02:16:19 +0200

    working base version

---
+++ init.lua
@@ -0,0 +1,60 @@
+require("config.lazy")
+vim.opt.shiftwidth = 4
+vim.opt.clipboard = "unnamedplus"
+
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":.lua<CR>")
+
+local opt = vim.opt
+
+----- Interesting Options -----
+
+-- You have to turn this one on :)
+opt.inccommand = "split"
+
+-- Best search settings :)
+opt.smartcase = true
+opt.ignorecase = true
+
+----- Personal Preferences -----
+opt.number = true
+opt.relativenumber = true
+
+opt.splitbelow = true
+opt.splitright = true
+
+opt.signcolumn = "yes"
+opt.shada = { "'10", "<0", "s10", "h" }
+
+opt.swapfile = false
+
+-- Don't have `o` add a comment
+opt.formatoptions:remove "o"
+
+opt.wrap = true
+opt.linebreak = true
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+
+opt.more = false
+
+opt.foldmethod = "manual"
+
+opt.title = true
+opt.titlestring = '%t%( %M%)%( (%{expand("%:~:h")})%)%a (nvim)'
+
+opt.undofile = true
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
